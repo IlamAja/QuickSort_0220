7a129fab4d53859fdab388cf79c2d76769e015f2
Ilam Ball <informasiilam@gmail.com>
2024-03-28 15:08:21 +0700

    membuat void input

--- quicksort.cpp
+++ quicksort.cpp
@@ -6,6 +6,30 @@ int cmp_count = 0;
 int mov_count = 0;
 int n;
 
+void input()
+{
+    while (true)
+    {
+        cout << "masukan banyaknya elemen array : ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
+    }
+
+    cout << "====================" << endl;
+    cout << "masukan elemen array" << endl;
+    cout << "====================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
+}
+
 int main(){
 
 }
